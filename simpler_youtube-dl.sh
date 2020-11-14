8a998a03f3b3e35124b35d34693a81fc0d5f9d9e
Andreas Araya Osorio <66529441+4sito@users.noreply.github.com>
2020-11-14 13:36:32 +0100

    Update simpler_youtube-dl.sh

--- simpler_youtube-dl.sh
+++ simpler_youtube-dl.sh
@@ -1,27 +1,32 @@
 #! /bin/bash
 
+m_quality="mp3"
+v_quality="mp4"
+m_location="~/Music"
+v_location="~/Video"
+
 PS3='Choose an action: '
 
-entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Playlist" "Fav Playlist" "Quit")
+entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Playlist" "Fav Playlist" "Config" "Quit")
 
 	select fav in "${entries[@]}"; do
 		case $fav in 
 			"Video Download")
 				echo input url:
 					read url
-				location="~/Video"
+			
 				echo starting download...
-					youtube-dl -f mp4 $url -w -o "$location/%(title)s.%(ext)s" -w
-				echo Thanks, you can find the downloaded file at $location, byee!
+					youtube-dl -f $v_quality $url -w -o "$v_location/%(title)s.%(ext)s" -w
+				echo Thanks, you can find the downloaded file at $v_location, byee! 
 				break
 				;;
 			"Audio Download") 
                                 echo input url: 
                                         read url
-                                location="~/Music" 
+                                
                                 echo starting download... 
-                                        youtube-dl -f mp3 $url -w  -o "$location/%(title)s.%(ext)s"
-                                echo Thanks, you can find the downloaded file at $location, byee!
+                                        youtube-dl -f $m_quality $url -w  -o "$m_location/%(title)s.%(ext)s"
+                                echo Thanks, you can find the downloaded file at $m_location, byee!
 				break
 				;;
                         "Playlist Download")
@@ -29,8 +34,8 @@ entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Pl
 					read url
 				location="~/Music" 
 				echo starting playlist download...
-					youtube-dl -f mp3 $url -w -o "$location/%(title)s.%(ext)s"
-                                echo Thanks, you can find the downloaded playlist songs at $location, bye bye!
+					youtube-dl -f $m_quality $url -w -o "$m_location/%(title)s.%(ext)s"
+                                echo Thanks, you can find the downloaded playlist songs at $m_location, bye bye!
 				break
 				;;
 			"Set favourite Playlist")
@@ -48,8 +53,8 @@ entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Pl
 				url=`cat $SAVED_PLAYLIST`
 				location="~/Music"
 				echo Starting Download of the saved playlist at $location...
-					youtube-dl -f mp3 $url  -w -o "$location/%(title)s.%(ext)s" 
-				echo Thanks, you can find the downloaded songs at $location, byee!
+					youtube-dl -f mp3 $url  -w -o "$m_location/%(title)s.%(ext)s" 
+				echo Thanks, you can find the downloaded songs at $m_location, byee!
 				break
 				;;
 				
