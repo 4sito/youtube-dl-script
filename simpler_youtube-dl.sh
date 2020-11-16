dd55150af4a8dc813a4b3c318b65b3b7551a179a
Andreas Araya Osorio <andreas.arayaosorio@outlook.com>
2020-11-16 01:03:27 +0100

    little changes to code

--- simpler_youtube-dl.sh
+++ simpler_youtube-dl.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+
+# setting default file formats and locations
+
 m_quality="mp3"
 v_quality="mp4"
 m_location="~/Music"
@@ -7,10 +10,11 @@ v_location="~/Video"
 
 PS3='Choose an action: '
 
-entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Playlist" "Fav Playlist" "Config" "Quit")
+entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Playlist" "Fav Playlist" "Quit")
 
 	select fav in "${entries[@]}"; do
 		case $fav in 
+
 			"Video Download")
 				echo input url:
 					read url
@@ -20,6 +24,7 @@ entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Pl
 				echo Thanks, you can find the downloaded file at $v_location, byee! 
 				break
 				;;
+
 			"Audio Download") 
                                 echo input url: 
                                         read url
@@ -29,15 +34,18 @@ entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Pl
                                 echo Thanks, you can find the downloaded file at $m_location, byee!
 				break
 				;;
-                        "Playlist Download")
+
+            "Playlist Download")
 				echo input url:
 					read url
+
 				location="~/Music" 
 				echo starting playlist download...
 					youtube-dl -f $m_quality $url -w -o "$m_location/%(title)s.%(ext)s"
-                                echo Thanks, you can find the downloaded playlist songs at $m_location, bye bye!
+                echo Thanks, you can find the downloaded playlist songs at $m_location, bye bye!
 				break
 				;;
+
 			"Set favourite Playlist")
 				echo Playlist url:
 					read url
@@ -48,13 +56,18 @@ entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Pl
 				echo $url >> saved_playlist
 				
 				;;				
+
 			"Fav Playlist")
+
 				SAVED_PLAYLIST="saved_playlist"
 				url=`cat $SAVED_PLAYLIST`
+
 				location="~/Music"
 				echo Starting Download of the saved playlist at $location...
 					youtube-dl -f mp3 $url  -w -o "$m_location/%(title)s.%(ext)s" 
+
 				echo Thanks, you can find the downloaded songs at $m_location, byee!
+
 				break
 				;;
 				
@@ -62,7 +75,9 @@ entries=("Video Download" "Audio Download" "Playlist Download" "Set favourite Pl
 				echo exiting...
 				exit
 				;;
+
 			*) echo Hippity, Hoppity, retry with a valid option
 				;;
+
 	esac
 done
